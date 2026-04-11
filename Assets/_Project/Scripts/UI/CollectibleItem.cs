9726f65d13df98ae54496e8e59eab688b3c0ff77
ReneBarrera16 <lizarazo611@gmail.com>
2026-04-11 00:40:26 -0500

    Drop System Fix

--- Assets/_Project/Scripts/UI/CollectibleItem.cs
+++ Assets/_Project/Scripts/UI/CollectibleItem.cs
@@ -4,17 +4,35 @@ public class CollectibleItem : MonoBehaviour
 {
     public ItemData item;
     public int amount = 1;
+    public bool hasLuch = true;
 
     private Rigidbody rb;
     private bool canBePickedUp = false;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        if (hasLuch)
+        {
+            rb = GetComponent<Rigidbody>();
         
-        Vector3 randomForce = new Vector3(Random.Range(-2f, 2f), Random.Range(4f, 6f), Random.Range(-2f, 2f));
-        rb.AddForce(randomForce, ForceMode.Impulse);
-        Invoke(nameof(EnablePickup), 0.5f);
+            Vector3 randomForce = new Vector3(Random.Range(-2f, 2f), Random.Range(4f, 6f), Random.Range(-2f, 2f));
+            rb.AddForce(randomForce, ForceMode.Impulse);
+            Invoke(nameof(EnablePickup), 0.5f);
+        }
+        else
+        {
+            canBePickedUp = true;
+        }
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        if (hasLuch && rb != null && !rb.isKinematic && collision.gameObject.CompareTag("Ground"))
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.isKinematic = true;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
