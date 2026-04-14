cb39b0e629a5000929978cf8f6b357c098aeabac
JuanDavidL <juan.laiseca98@gmail.com>
2026-04-14 11:26:06 -0500

    Fix Update Scripts

--- Assets/_Project/Scripts/Core/Managers/InventoryManager.cs
+++ Assets/_Project/Scripts/Core/Managers/InventoryManager.cs
@@ -1,24 +1,29 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
 
-    [System.Serializable] public class InventorySlot
+    [System.Serializable]
+    public class InventorySlot
     {
         public ItemData item;
         public int quantity;
     }
 
     public List<InventorySlot> inventory = new List<InventorySlot>();
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // evento para que el sistema de pociones se actualice visualmente
+    public event Action OnInventoryUpdated;
+
     void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);  
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -26,7 +31,7 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+
     public void AddItem(ItemData newItem, int amount = 1)
     {
         InventorySlot existing = inventory.Find(slot => slot.item == newItem);
@@ -37,6 +42,23 @@ public class InventoryManager : MonoBehaviour
         else
         {
             inventory.Add(new InventorySlot { item = newItem, quantity = amount });
-        }   
+        }
+
+        OnInventoryUpdated?.Invoke(); // Notificar cambio
+    }
+
+    // MÉTODO para que "sacar" cosas a la mesa 3D
+    public bool RemoveItem(ItemData itemToRemove, int amount)
+    {
+        InventorySlot existing = inventory.Find(slot => slot.item == itemToRemove);
+        if (existing != null && existing.quantity >= amount)
+        {
+            existing.quantity -= amount;
+            if (existing.quantity <= 0) inventory.Remove(existing);
+
+            OnInventoryUpdated?.Invoke();
+            return true; // Se pudo sacar el material
+        }
+        return false; // No hay suficiente material
     }
 }
