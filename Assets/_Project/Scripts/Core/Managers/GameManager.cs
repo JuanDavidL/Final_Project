5a5265edfb5236deda138ad2e3f70cbcf880946f
Juan Aragón <juansa0017@gmail.com>
2026-04-25 16:16:55 -0500

    Updated GameManger
    Tutorial will restart when we press play in Unity

--- Assets/_Project/Scripts/Core/Managers/GameManager.cs
+++ Assets/_Project/Scripts/Core/Managers/GameManager.cs
@@ -64,6 +64,9 @@ public class GameManager : MonoBehaviour
         fireballUpgradesPurchased = 0;
         frostNovaUpgradesPurchased = 0;
         totalCredits = 10000; // créditos de prueba
+        PlayerPrefs.DeleteKey("TutorialVisto");
+        isTutorialCompleted = false;
+
         SaveGlobalProgress(); // ✅ guarda el reset en PlayerPrefs inmediatamente
         //Debug.Log($"Reseteado! Verificando PlayerPrefs → Fireball: {PlayerPrefs.GetInt("FireballUpgrades")} | FrostNova: {PlayerPrefs.GetInt("FrostNovaUpgrades")}");
     }
